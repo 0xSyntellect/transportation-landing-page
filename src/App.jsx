b7feba9565ee3dbe2e922c3576942323bb925306
0xsyntellect <0xsyntellect@gmail.com>
2025-02-06 10:38:41 +0300

    remove team, small adjustments

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,6 @@ import { About } from "./components/about";
 import { Services } from "./components/services";
 import { Gallery } from "./components/gallery";
 import { Testimonials } from "./components/testimonials";
-import { Team } from "./components/Team";
 import { Contact } from "./components/contact";
 import JsonData from "./data/data.json";
 import SmoothScroll from "smooth-scroll";
@@ -42,7 +41,6 @@ const App = () => {
               <Services data={landingPageData.Services} />
               <Gallery data={landingPageData.Gallery} />
               <Testimonials data={landingPageData.Testimonials} />
-              <Team data={landingPageData.Team} />
               <Contact data={landingPageData.Contact} />
             </>
           }
