ae07bc05362bd16703db24b18eb54dd6fda2bda7
0xsyntellect <0xsyntellect@gmail.com>
2025-02-20 00:10:31 +0300

    fixed booking form

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,27 +1,32 @@
-// src/components/header.jsx
+// src/components/Header.jsx
 import React from "react";
 import { Booking } from "./Booking";
+import "./Header.css";
+import introBg from "../img/intro-bg.png"; // Make sure intro-bg.png is in src/img/
 
 export const Header = (props) => {
   return (
-    <header id="header">
+    <header
+      id="header"
+      style={{
+        background: `url(${introBg}) center center no-repeat`,
+        backgroundSize: "cover",
+      }}
+    >
       <div className="intro">
         <div className="overlay">
           <div className="container">
             {/* Header Title and Paragraph */}
             <div className="row">
               <div className="col-md-8 col-md-offset-2 intro-text">
-                <h1>
-                  {props.data ? props.data.title : "Loading"}
-                </h1>
-                <p>
-                  {props.data ? props.data.paragraph : "Loading"}
-                </p>
+                <h1>{props.data ? props.data.title : "Loading"}</h1>
+                <p>{props.data ? props.data.paragraph : "Loading"}</p>
               </div>
             </div>
-            {/* Booking form in its own row */}
+            {/* Booking form row */}
             <div className="row">
-              <div className="col-md-8 col-md-offset-2">
+              {/* Use a wider column to allow the booking form to expand */}
+              <div className="col-md-12">
                 <Booking />
               </div>
             </div>
